3f697ae0c6d96927b5f1e59e33a76f4b127137e6
John Cranney <john.cranney@gmail.com>
2023-10-08 21:41:28 +1100

    Added ntp

--- src/Network.cpp
+++ src/Network.cpp
@@ -1,12 +1,26 @@
 #include "Logging.h"
 #include <WiFiManager.h>
 WiFiManager wifiManager;
+
 #include <Preferences.h>
+#include <time.h>
+
 // #include <WiFi.h>
+
+const char *ntpServer = "pool.ntp.org";
 
 #define HOMEWIFISSID "HOMEWIFISSID"
 #define HOMEWIFIPASS "HOMEWIFIPASS"
-
+void printLocalTime() {
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo,
+                 "%A, %B %d %Y %H:%M:%S"); // Print the current time in a
+                                           // human-readable format
+}
 void setupWifi(Preferences &prefs) {
   // prefs.putString(HOMEWIFISSID,"");
   // prefs.putString(HOMEWIFIPASS, "");
@@ -23,9 +37,9 @@ void setupWifi(Preferences &prefs) {
   }
   if (prefs.isKey(HOMEWIFISSID) && prefs.isKey(HOMEWIFIPASS)) {
     log("Connnecting to home wifi...");
-   
-     WiFi.begin(
-        prefs.getString(HOMEWIFISSID).c_str(), prefs.getString(HOMEWIFIPASS).c_str());
+
+    WiFi.begin(prefs.getString(HOMEWIFISSID).c_str(),
+               prefs.getString(HOMEWIFIPASS).c_str());
     while (WiFi.status() != WL_CONNECTED) {
       delay(1000);
       log("Waiting for connection...");
@@ -34,6 +48,9 @@ void setupWifi(Preferences &prefs) {
     // Once connected, log the IP address
     IPAddress ip = WiFi.localIP();
     log("Connected! IP address: %d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
+    // Init and get the time
+    configTime(0, 0, ntpServer);
+    printLocalTime();
 
   } else {
     log("No wifi details in prefs");
